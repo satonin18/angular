76fe7e2c1c0df84aadf16966b9dabc7fec1835ac
satonin18 <satonin18@mail.ru>
2019-08-28 14:29:58 +0500

    tsc app.ts (comandLine)
    https://metanit.com/web/typescript/1.2.php

---
+++ helloapp/app.js
@@ -0,0 +1,10 @@
+var el = this.document.getElementById("content");
+var User = /** @class */ (function () {
+    function User(_name, _age) {
+        this.name = _name;
+        this.age = _age;
+    }
+    return User;
+}());
+var tom = new User("Том", 29);
+el.innerHTML = "Имя: " + tom.name + " возраст: " + tom.age;
