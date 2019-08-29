645a033c57d67361bb076fc04cadd41376fa8cf6
satonin18 <satonin18@mail.ru>
2019-08-29 18:21:12 +0500

    <ng-content> in chield_Html_Template
    https://metanit.com/web/angular2/2.6.php

--- helloapp/src/app/app.component.ts
+++ helloapp/src/app/app.component.ts
@@ -1,35 +1,11 @@
 import { Component } from '@angular/core';
-     
+      
 @Component({
     selector: 'my-app',
-    // template: `<label>Введите имя:</label>
-    //              <input [(ngModel)]="name" placeholder="name">
-    //              <h1>Добро пожаловать {{name}}!</h1>`
-    // templateUrl: './app.component.html' 
-    // ,
-    // styles: [` 
-    //         h1, h2{color:navy;}
-    //         p{font-size:13px; font-family:Verdana;}
-    //         :host {
-    //             font-family: Verdana;
-    //             color: #555;
-    //         }
-    // `]
-    // styleUrls: ['./app.component.css']
-
-    // Привязка к классам CSS
-    // [class]="varStrContain_nameClass"
-    template: `<div [class]="varStrContain_nameClass"></div>
-    <br>
-    <div [style.backgroundColor]="isRed? 'red' : 'green'"></div>
-    <div [style.background-color]="!isRed ? 'red' : 'green'"></div>
-    <input type="checkbox" [(ngModel)]="isRed" />`,
-    styles: [`
-        div {width:50px; height:50px; border:1px solid #ccc}
-         .isredbox{background-color:red;}
-    `]
+    template: `<child-comp> <h2>Добро пожаловать {{name}}!</h2> </child-comp>
+                <p>By chield</p>`,
+    styles: [`h2, p {color:#333;}`]
 })
-export class AppComponent{ 
-    varStrContain_nameClass = "isredbox"
-    isRed = false;
+export class AppComponent { 
+    name = 'Tom';
 }
--- helloapp/src/app/app.module.ts
+++ helloapp/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { ChildComponent }   from './child.component';
 
 @NgModule({
     imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent ],
-    bootstrap:    [ AppComponent ] //on start
+    declarations: [ AppComponent, ChildComponent ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
 /*
