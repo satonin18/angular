c70348838270868ef41c71d954a5fe8f9dbff8cd
satonin18 <satonin18@mail.ru>
2019-08-29 18:00:30 +0500

    Привязка к классам CSS

--- helloapp/src/app/app.component.ts
+++ helloapp/src/app/app.component.ts
@@ -5,14 +5,8 @@ import { Component } from '@angular/core';
     // template: `<label>Введите имя:</label>
     //              <input [(ngModel)]="name" placeholder="name">
     //              <h1>Добро пожаловать {{name}}!</h1>`
-    // templateUrl: './app.component.html'
-
-    template: 
-    `<p>Привет {{name}}</p>
-                <input type="text" [(ngModel)]="name" /> <br><br>
-                <input type="text" [(ngModel)]="name" />`
-                
-    ,
+    // templateUrl: './app.component.html' 
+    // ,
     // styles: [` 
     //         h1, h2{color:navy;}
     //         p{font-size:13px; font-family:Verdana;}
@@ -21,9 +15,21 @@ import { Component } from '@angular/core';
     //             color: #555;
     //         }
     // `]
-    styleUrls: ['./app.component.css']
-    
+    // styleUrls: ['./app.component.css']
+
+    // Привязка к классам CSS
+    // [class]="varStrContain_nameClass"
+    template: `<div [class]="varStrContain_nameClass"></div>
+    <br>
+    <div [style.backgroundColor]="isRed? 'red' : 'green'"></div>
+    <div [style.background-color]="!isRed ? 'red' : 'green'"></div>
+    <input type="checkbox" [(ngModel)]="isRed" />`,
+    styles: [`
+        div {width:50px; height:50px; border:1px solid #ccc}
+         .isredbox{background-color:red;}
+    `]
 })
-export class AppComponent { 
-    name:string = "Tom";
+export class AppComponent{ 
+    varStrContain_nameClass = "isredbox"
+    isRed = false;
 }
