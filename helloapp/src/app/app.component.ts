334107e46179948e01a52149b1ed88f0382dade4
satonin18 <satonin18@mail.ru>
2019-08-29 17:51:51 +0500

    односторонняя Привязка
    https://metanit.com/web/angular2/2.5.php

--- helloapp/src/app/app.component.ts
+++ helloapp/src/app/app.component.ts
@@ -5,7 +5,28 @@ import { Component } from '@angular/core';
     // template: `<label>Введите имя:</label>
     //              <input [(ngModel)]="name" placeholder="name">
     //              <h1>Добро пожаловать {{name}}!</h1>`
-    templateUrl: './app.component.html'
+    // templateUrl: './app.component.html'
+
+    template: 
+    // `<p>Имя: {{name}}</p>
+    // // <p>Возраст: {{age}}</p>`
+
+    // `<input type="text"
+    // [value]="name" />` //*!!!
+    // //value="{{name}}" />`
+
+    // `<p [textContent]="name"></p>` //У <p> нет атрибута textContent. Зато у интерфейса Node есть
+
+    // `<table border="1">
+    //             <tr><td [attr.colspan]="colspanVar">One-Two</td></tr>
+    //             <tr><td>Three</td><td>Four</td></tr>
+    // </table>`
+
+    `<p>Количество кликов {{count}}</p>`+
+    // `<button (click)="increase()"> Click </button>`
+    // `<button on-click="increase()">Click</button>`
+    `<button (click)="increase($event)">Click</button>`
+                
     ,
     // styles: [` 
     //         h1, h2{color:navy;}
@@ -19,5 +40,18 @@ import { Component } from '@angular/core';
     
 })
 export class AppComponent { 
-    name: "";
+    // name: "";
+    name = "Tom";
+    age = 25;
+
+    colspanVar = 2;
+
+    count: number=0;
+    // increase() : void {
+    //     this.count++;
+    // }
+    increase($event : any) : void {
+        this.count++;
+        console.log($event);
+    }
 }
