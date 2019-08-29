a9e9fcf7f6f80731a841069a0e342a428f2a6481
satonin18 <satonin18@mail.ru>
2019-08-29 19:20:24 +0500

    @ViewChild (ChildrenComponent)

--- helloapp/src/app/app.component.ts
+++ helloapp/src/app/app.component.ts
@@ -1,20 +1,17 @@
-import { Component } from '@angular/core';
-
-// @Component({
-//     selector: 'my-app',
-//     template: `
-//                 <p #userName>{{name}}</p>
-//                 <p>{{userName.textContent}}</p>
-//                 <input type="text" [(ngModel)]="name" />`
-// })
-// export class AppComponent { 
-//     name:string="Tom";
-// }
-
+import { Component, ViewChild } from '@angular/core';
+import { ChildComponent} from './child.component';
+       
 @Component({
     selector: 'my-app',
-    template: `<child-comp #counter></child-comp>
-                <button (click)="counter.increment()">+</button>
-                <button (click)="counter.decrement()">-</button>`
+    template: `<child-comp></child-comp>
+                <button (click)="increment()">+</button>
+                <button (click)="decrement()">-</button>`
 })
-export class AppComponent { }
+export class AppComponent { 
+ 
+    @ViewChild(ChildComponent, {static: false})
+    private counterComponent: ChildComponent;
+     
+    increment() { this.counterComponent.increment(); }
+    decrement() { this.counterComponent.decrement(); }
+}
